d2e9e56419ac9e8f5d1228cc3d259c110f4b1b9d
Akhsarbek Gozoev <Akhsarbek.Gozoev@norgesgruppen.no>
2024-05-08 10:42:31 +0200

    More generic solution allowing for multiple rules

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
-﻿foreach (var i in Enumerable.Range(1, 100))
+﻿using FizzBuzz;
+
+static void PrintResults(IEnumerable<int> numbers, IEnumerable<Rule> rules)
 {
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.WriteLine("Fizz Buzz");
-    else if (i % 3 == 0)
-        Console.WriteLine("Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine("Buzz");
-    else Console.WriteLine(i);
+    foreach (var i in numbers)
+    {
+        bool anyRuleApplied = false;
+        foreach (var rule in rules)
+        {
+            if (rule.Applies(i))
+            {
+                anyRuleApplied = true;
+                Console.WriteLine(rule.Code);
+            }
+        }
+
+        if (!anyRuleApplied) Console.WriteLine(i);
+    }
 }
 
-foreach (var j in Enumerable.Range(0, 100))
-{
-    var i = 100 - j;
+var FizzRule = new Rule("Fizz", 3);
+var BuzzRule = new Rule("Buzz", 5);
+var FizzBuzzRule = new Rule("Fizz Buzz", 3, 5);
+PrintResults(Enumerable.Range(1, 100), [FizzBuzzRule, FizzRule, BuzzRule]); // Viktig med rekkefølgen på regler i listen
 
-    if (i % 4 == 0 && i % 9 == 0)
-        Console.WriteLine("Jazz Fuzz");
-    else if (i % 9 == 0)
-        Console.WriteLine("Jazz");
-    else if (i % 4 == 0)
-        Console.WriteLine("Fuzz");
-    else Console.WriteLine(i);
-}
+var JazzRule = new Rule("Jazz", 9);
+var FuzzRule = new Rule("Fuzz", 4);
+var JazzFuzzRule = new Rule("Fuzz", 4, 9);
+PrintResults(Enumerable.Range(1, 100).Reverse(), [JazzFuzzRule, JazzRule, FuzzRule]);  // Viktig med rekkefølgem på regler i listen
---
+++ Rule.cs
@@ -0,0 +1,7 @@
+﻿namespace FizzBuzz;
+
+internal record Rule(string Code, params int[] Devisors)
+{
+    public bool Applies(int divident) =>
+        Devisors.All(devisor => divident % devisor == 0);
+}
