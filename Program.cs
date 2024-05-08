ba9b54045794c42292749a44cb81288066bef1e2
Akhsarbek Gozoev <Akhsarbek.Gozoev@norgesgruppen.no>
2024-05-08 11:05:51 +0200

    trenger norskkurs

--- Program.cs
+++ Program.cs
@@ -21,9 +21,9 @@ static void PrintResults(IEnumerable<int> numbers, IEnumerable<Rule> rules)
 var FizzRule = new Rule("Fizz", 3);
 var BuzzRule = new Rule("Buzz", 5);
 var FizzBuzzRule = new Rule("Fizz Buzz", 3, 5);
-PrintResults(Enumerable.Range(1, 100), [FizzBuzzRule, FizzRule, BuzzRule]); // Viktig med rekkefølgen på regler i listen
+PrintResults(Enumerable.Range(1, 100), [FizzBuzzRule, FizzRule, BuzzRule]); // Viktig med rekkefølgen på reglene i listen
 
 var JazzRule = new Rule("Jazz", 9);
 var FuzzRule = new Rule("Fuzz", 4);
 var JazzFuzzRule = new Rule("Fuzz", 4, 9);
-PrintResults(Enumerable.Range(1, 100).Reverse(), [JazzFuzzRule, JazzRule, FuzzRule]);  // Viktig med rekkefølgem på regler i listen
+PrintResults(Enumerable.Range(1, 100).Reverse(), [JazzFuzzRule, JazzRule, FuzzRule]);  // Viktig med rekkefølgem på reglene i listen
